0d5013c15858d7ed79047724e95cef29561e0e16
panhao <4812ph@163.com>
2016-11-14 16:52:38 +0800

    20161114

--- ZianLauncher/Form1.Designer.cs
+++ ZianLauncher/Form1.Designer.cs
@@ -38,7 +38,9 @@
             this.button6 = new System.Windows.Forms.Button();
             this.label4 = new System.Windows.Forms.Label();
             this.textBoxPassword = new System.Windows.Forms.TextBox();
-            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBoxRootPath = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // textBoxJava
@@ -49,7 +51,6 @@
             this.textBoxJava.Name = "textBoxJava";
             this.textBoxJava.Size = new System.Drawing.Size(353, 40);
             this.textBoxJava.TabIndex = 0;
-            this.textBoxJava.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // button1
             // 
@@ -60,7 +61,6 @@
             this.button1.TabIndex = 1;
             this.button1.Text = "Search";
             this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // textBoxID
             // 
@@ -115,9 +115,9 @@
             // 
             this.button6.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
             this.button6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button6.Location = new System.Drawing.Point(412, 89);
+            this.button6.Location = new System.Drawing.Point(7, 141);
             this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(84, 72);
+            this.button6.Size = new System.Drawing.Size(489, 44);
             this.button6.TabIndex = 11;
             this.button6.Text = "Start";
             this.button6.UseVisualStyleBackColor = false;
@@ -143,21 +143,44 @@
             this.textBoxPassword.TabIndex = 12;
             this.textBoxPassword.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
-            // treeView1
+            // label5
             // 
-            this.treeView1.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.treeView1.Location = new System.Drawing.Point(6, 88);
-            this.treeView1.Name = "treeView1";
-            this.treeView1.Size = new System.Drawing.Size(399, 72);
-            this.treeView1.TabIndex = 14;
-            this.treeView1.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterSelect);
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label5.Location = new System.Drawing.Point(-2, 101);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(80, 19);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "MC PATH";
+            // 
+            // textBoxRootPath
+            // 
+            this.textBoxRootPath.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBoxRootPath.Location = new System.Drawing.Point(84, 89);
+            this.textBoxRootPath.Multiline = true;
+            this.textBoxRootPath.Name = "textBoxRootPath";
+            this.textBoxRootPath.Size = new System.Drawing.Size(322, 44);
+            this.textBoxRootPath.TabIndex = 16;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Location = new System.Drawing.Point(412, 89);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(84, 44);
+            this.button2.TabIndex = 17;
+            this.button2.Text = "Search";
+            this.button2.UseVisualStyleBackColor = false;
             // 
             // ZianLauncher
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(503, 174);
-            this.Controls.Add(this.treeView1);
+            this.ClientSize = new System.Drawing.Size(503, 194);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBoxRootPath);
+            this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.textBoxPassword);
             this.Controls.Add(this.button6);
@@ -173,7 +196,6 @@
             this.Name = "ZianLauncher";
             this.ShowIcon = false;
             this.Text = "ZianLauncher";
-            this.Load += new System.EventHandler(this.ZianLauncher_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -191,7 +213,9 @@
         private System.Windows.Forms.Button button6;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.TextBox textBoxPassword;
-        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBoxRootPath;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- ZianLauncher/Form1.cs
+++ ZianLauncher/Form1.cs
@@ -1,57 +1,115 @@
 ﻿
 using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace ZianLauncher
 {
+
     public partial class ZianLauncher : Form
     {
-        string ID = "null";
+        public List<string> Libraries = new List<string>();
+        string ID = "zian1";
         string Password = "null";
         string RAM = "2048";
+        string GameRootPath = @"C:\Users\Administrator\Desktop\MEP\minecraft1.8.8";//System.Environment.CurrentDirectory;
+        string JavaPath = @"C:\Program Files\Java\jre1.8.0_111\bin\javaw.exe";
+        Global P = new Global(true);
+
+        public static void _SearchFiles(string dir, string type, ref List<string> output)
+        {
+            if (Directory.Exists(dir))
+            {
+                foreach (string d in Directory.GetFileSystemEntries(dir))
+                {
+                    if (File.Exists(d))
+                    {
+                        string name = Path.GetFileNameWithoutExtension(d);
+                        string extension = Path.GetExtension(d);
+
+                        if (extension == type) { output.Add(d); }
+                    }
+                    else
+                        _SearchFiles(d, type, ref output);
+                }
+            }
+        }
+
+        public string ToArguments()
+        {
+            _SearchFiles(this.GameRootPath + @"\.minecraft\libraries", ".jar", ref Libraries);
+            StringBuilder builder = new StringBuilder();
+            builder.Append("-Xincgc");
+            builder.Append(" -Xms512M");
+            builder.Append(" -Xmx").Append(this.RAM).Append("M");
+            builder.Append(" -Djava.library.path=" + this.GameRootPath + @"\.minecraft\versions\1.8.8-forge1.8.8-11.15.0.1655\1.8.8-forge1.8.8-11.15.0.1655-natives-1479119743");
+            builder.Append(" -classpath ");
+            foreach (string str2 in this.Libraries)
+            {
+                builder.Append(str2).Append(';');
+            }
+            builder.Append(this.GameRootPath + @"\.minecraft\versions\1.8.8\1.8.8.jar");
+            builder.Append(" net.minecraft.launchwrapper.Launch");
+            builder.Append(" --username " + ID);
+            builder.Append(" --version 1.8.8-forge1.8.8-11.15.0.1655");
+            builder.Append(" --gameDir " + this.GameRootPath + @"\.minecraft");
+            builder.Append(" --assetsDir " + this.GameRootPath + @"\.minecraft\assets");
+            builder.Append(" --assetIndex 1.8");
+            builder.Append(" --uuid ${auth_uuid}");
+            builder.Append(" --accessToken ${auth_access_token}");
+            builder.Append(" --userProperties {}");
+            builder.Append(" --userType Legacy");
+            builder.Append(" --tweakClass net.minecraftforge.fml.common.launcher.FMLTweaker");
+            return builder.ToString();
+        }
+
+
         public ZianLauncher()
         {
             InitializeComponent();
             this.textBoxID.Text = ID;
             this.textBoxPassword.Text = Password;
             this.textBoxRAM.Text = RAM;
-        }
-
-      
-
-        private void textBox1_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void button5_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void button4_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void ZianLauncher_Load(object sender, EventArgs e)
-        {
-
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-
+            this.textBoxRootPath.Text = GameRootPath;
+            this.textBoxJava.Text = JavaPath;
+           
         }
 
         private void button6_Click(object sender, EventArgs e)
         {
-           
-        }
-
-        private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
-        {
-
+            
+            string ID = this.textBoxID.Text;
+            string Password = this.textBoxPassword.Text;
+            string RAM = this.textBoxRAM.Text;
+            string GameRootPath = this.textBoxRootPath.Text;
+            string JavaPath = this.textBoxJava.Text;
+            try
+            {
+                
+                ProcessStartInfo startInfo = new ProcessStartInfo(this.JavaPath)
+                {
+                    Arguments = ToArguments(),
+                    UseShellExecute = false,
+                    CreateNoWindow = false,
+                    WorkingDirectory = this.GameRootPath,
+                    RedirectStandardError = true,
+                    RedirectStandardOutput = true
+                };
+                Process javaprocess = Process.Start(startInfo);                         
+                string output = javaprocess.StandardOutput.ReadToEnd();
+                javaprocess.WaitForExit();//等待程序执行完退出进程
+                javaprocess.Close();
+                P.Print(output);
+            }
+            catch (Exception ex)
+            {
+                P.Print(ex.ToString());
+            }
         }
     }
 }
